7e79722c12d3fdc501aa6917a217e3e6f0d673dd
mrmadhat <daniel@relativemarketing.co.uk>
2020-04-23 17:51:13 +0100

    husky setup

--- src/Game.js
+++ src/Game.js
@@ -6,7 +6,7 @@ function Game() {
   return (
     <div className="Game">
       <header className="Game-header">
-        <Button onClick={() => alert("hello")}>New Word</Button>
+        <Button onClick={() => alert("hello")}>New </Button>
       </header>
     </div>
   )
