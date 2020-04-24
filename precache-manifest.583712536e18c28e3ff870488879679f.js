7aa7ae9fd65cf458893f16cac67c9d34b80bb143
mrmadhat <daniel@relativemarketing.co.uk>
2020-04-24 11:43:21 +0100

    Updates

---
+++ precache-manifest.583712536e18c28e3ff870488879679f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44dbc605b1bccdbdad48260cff3bcac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18e1e29e94e8a8afdf84",
+    "url": "/static/css/main.3adbdeba.chunk.css"
+  },
+  {
+    "revision": "f1124c0c40e092a30362",
+    "url": "/static/js/2.e7efd7cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7efd7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e1e29e94e8a8afdf84",
+    "url": "/static/js/main.7637ac87.chunk.js"
+  },
+  {
+    "revision": "32a91c52483abc33d3b4",
+    "url": "/static/js/runtime-main.5bfa655a.js"
+  }
+]);
