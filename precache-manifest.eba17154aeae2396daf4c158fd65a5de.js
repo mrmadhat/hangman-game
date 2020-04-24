d489e40ba6a609a10b6ecf0b32b1cbd73be94fc0
mrmadhat <daniel@relativemarketing.co.uk>
2020-04-24 10:56:36 +0100

    Updates

---
+++ precache-manifest.eba17154aeae2396daf4c158fd65a5de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb72610eb9c13f3d98e8469570c70e29",
+    "url": "/hangman-game/index.html"
+  },
+  {
+    "revision": "5f213fe0246e6ecc1132",
+    "url": "/hangman-game/static/css/main.fefe5d40.chunk.css"
+  },
+  {
+    "revision": "f1124c0c40e092a30362",
+    "url": "/hangman-game/static/js/2.e7efd7cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-game/static/js/2.e7efd7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f213fe0246e6ecc1132",
+    "url": "/hangman-game/static/js/main.1f2e9dc4.chunk.js"
+  },
+  {
+    "revision": "5df91aea219a309e0071",
+    "url": "/hangman-game/static/js/runtime-main.1ba09430.js"
+  }
+]);
